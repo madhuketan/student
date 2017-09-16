796768fd1d8115d00e55652e8035a8394043a979
madhuketan <31992446+madhuketan@users.noreply.github.com>
2017-09-16 12:03:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Scanner sn= new Scanner(System.in);
+		int n,i
+		int student[]=new student[];
+		System.out.println("enter how may students");
+		for(i=0;i<n;i++)
+			n=sn.nextInt();
+
+	}
+
+}
